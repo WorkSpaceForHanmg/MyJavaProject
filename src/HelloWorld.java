c3e498d8b48f57f7d5ac60526405525b84bd1dd8
WorkSpaceForHanmg <dlzkfmtm11@naver.com>
2025-04-23 10:52:38 +0900

    init file

---
+++ src/HelloWorld.java
@@ -0,0 +1,12 @@
+/*
+ *javac HelloWorld.java
+ *java HelloWorld
+ *Run 실행 단축키 : Ctrl + f11
+ *우클릭 Run As - 
+ */
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Hellow Java!!");
+	}
+}
